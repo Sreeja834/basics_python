a2a89f544b989111c758d3a828f1df40e4e2734c
Sreeja834 <madasusreeja369@gmail.com>
2024-11-25 20:44:15 +0530

    Add files via upload

---
+++ multiplication.py
@@ -0,0 +1,11 @@
+print("***Multiplication Table***")
+while True:
+    n = int(input("Enter a number"))
+    if(n==0)
+    print("Thank You!!!")
+    break
+else:
+    for i in range (1,11)
+    ans = n * i
+    print(f"{n} * {i}={ans}")
+    
